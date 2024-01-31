5980eaa31c3ed264db5a073b35cc49a5152d9b5b
veraaccion <140852436+veraaccion@users.noreply.github.com>
2024-01-31 10:38:20 +0530

    Implemented Annotation Object to display annotations

--- veracode-helper/generate_signature.js
+++ veracode-helper/generate_signature.js
@@ -1,37 +1,4 @@
 const crypto = require('crypto');
-const preFix = "VERACODE-HMAC-SHA-256";
-const verStr = "vcode_request_version_1";
-
-function hmac256(data, key, format) {
-    var hash = crypto.createHmac('sha256', key).update(data);
-    // no format = Buffer / byte array
-    return hash.digest(format);
-}
-
-function getByteArray(hex) {
-    var bytes = [];
-    for (var i = 0; i < hex.length - 1; i += 2) {
-        bytes.push(parseInt(hex.substr(i, 2), 16));
-    }
-    // signed 8-bit integer array (byte array)
-    return Int8Array.from(bytes);
-}
-
-function generateHeader(url, method, host, id, key) {
-    var data = `id=${id}&host=${host}&url=${url}&method=${method}`;
-    var timestamp = (new Date().getTime()).toString();
-    var nonce = crypto.randomBytes(16).toString("hex");
-    // calculate signature
-    var hashedNonce = hmac256(getByteArray(nonce), getByteArray(key), null);
-    var hashedTimestamp = hmac256(timestamp, hashedNonce, null);
-    var hashedVerStr = hmac256(verStr, hashedTimestamp, null);
-    var signature = hmac256(data, hashedVerStr, 'hex');
-    var output = `${preFix} id=${id},ts=${timestamp},nonce=${nonce},sig=${signature}`;
-    console.log(output);
-}
-
-/*jshint esversion: 6 */
-
 var url = require('url');
 
 const authorizationScheme = 'VERACODE-HMAC-SHA-256';
